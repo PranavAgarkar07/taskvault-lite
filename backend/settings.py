1ec7286e37103e85c9873c00fa6eec3a5b45a055
PranavAgarkar07 <pranavagarkar8@gmail.com>
2025-11-07 19:21:24 +0530

    connected cloud db

--- backend/settings.py
+++ backend/settings.py
@@ -15,6 +15,10 @@ from cryptography.fernet import Fernet
 import os
 from datetime import timedelta
 import dj_database_url
+from dotenv import load_dotenv
+load_dotenv()
+
+
 # Load encryption key from environment variable
 FERNET_KEY = os.environ.get("FERNET_KEY")
 
@@ -136,17 +140,17 @@ ACCOUNT_AUTHENTICATED_LOGIN_REDIRECTS = True
 ACCOUNT_ADAPTER = "api.adapter.CustomAccountAdapter"
 
 CORS_ALLOWED_ORIGINS = os.environ.get(
-    "CORS_ALLOWED_ORIGINS", "https://taskvault-lite.vercel.app/"
+    "CORS_ALLOWED_ORIGINS", "https://taskvault-lite.vercel.app"
 ).split(",")
 
 CSRF_TRUSTED_ORIGINS = os.environ.get(
-    "CSRF_TRUSTED_ORIGINS", "https://taskvault-lite.vercel.app/"
+    "CSRF_TRUSTED_ORIGINS", "https://taskvault-lite.vercel.app"
 ).split(",")
 
 # CORS_ALLOW_ALL_ORIGINS = True
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',  # 👈 Add this line at the top
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -154,13 +158,15 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware'
+    'allauth.account.middleware.AccountMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 
 CORS_ALLOWED_ORIGINS = [
     # "http://localhost:5173",
-    "https://taskvault-lite.vercel.app/"
+    "https://taskvault-lite.vercel.app"
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -185,18 +191,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-DATABASE_URL = os.environ.get("DATABASE_URL")
-if DATABASE_URL:
-    DATABASES = {"default": dj_database_url.parse(DATABASE_URL)}
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
-
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
@@ -239,3 +240,4 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
