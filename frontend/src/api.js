7a91755a88d6cbc8d6b6effa649e161c47ea9d6d
PranavAgarkar07 <pranavagarkar8@gmail.com>
2025-11-07 20:55:55 +0530

    dynamic username welcome

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,19 +1,7 @@
-import axios from "axios";
+import axios from 'axios'
 
-// ✅ Use environment variable if defined (good for Vercel)
-const BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "https://taskvault-lite.onrender.com/";
-
-// ✅ Create axios instance
-const API = axios.create({
-  baseURL: BASE_URL,
-});
-
-// ✅ Automatically attach JWT token for all requests
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
+const API=axios.create({
+    baseURL:"https://taskvault-lite.onrender.com/api/"
 });
 
 export default API;
